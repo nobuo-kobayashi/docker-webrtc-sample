b43b59f8f31cd5d2cf483b9f0ec7e452778c311c
小林伸郎 <nobu@gclue.jp>
2022-07-28 12:31:30 +0900

    network_mode: host をやめて、ports で使用するポート番号を指定するように修正

--- webrtc-sample/gst-webrtc-sample/src/main.cc
+++ webrtc-sample/gst-webrtc-sample/src/main.cc
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 {
   gst_init(&argc, &argv);
 
-  std::string url = "ws://127.0.0.1:9449/";
+  std::string url = "ws://signaling:9449/";
   std::string origin = "localhost";
 
   WebRTCMain main;
